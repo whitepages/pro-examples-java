246d9af70964becd2d5eaf4e563ee6208ba49daa
Sergei Akulich <sakulich@gmail.com>
2016-12-21 14:47:21 -0800

    switch version to 3.2
    upgrade to latest version, remove name checks from response parsing

--- src/main/java/IdentityCheckGet.java
+++ src/main/java/IdentityCheckGet.java
@@ -16,22 +16,22 @@ public class IdentityCheckGet {
         URI uri = new URIBuilder()
                 .setScheme("https")
                 .setHost("proapi.whitepages.com")
-                .setPath("/3.1/identity_check.json")
+                .setPath("/3.2/identity_check.json")
                 .setParameter("api_key", System.getenv("ID_CHECK_API_KEY"))
-                .setParameter("billing.name", "Drama Number")
-                .setParameter("billing.phone", "6464806649")
-                .setParameter("billing.address.street_line_1", "302 Gorham Ave")
-                .setParameter("billing.address.city", "Ashland")
-                .setParameter("billing.address.state_code", "MT")
-                .setParameter("billing.address.postal_code", "59004")
-                .setParameter("billing.address.country_code", "US")
-                .setParameter("shipping.name", "Drama Number")
-                .setParameter("shipping.phone", "6464806649")
-                .setParameter("shipping.address.street_line_1", "302 Gorham Ave")
-                .setParameter("shipping.address.city", "Ashland")
-                .setParameter("shipping.address.state_code", "MT")
-                .setParameter("shipping.address.postal_code", "59004")
-                .setParameter("shipping.address.country_code", "US")
+                .setParameter("primary.name", "Drama Number")
+                .setParameter("primary.phone", "6464806649")
+                .setParameter("primary.address.street_line_1", "302 Gorham Ave")
+                .setParameter("primary.address.city", "Ashland")
+                .setParameter("primary.address.state_code", "MT")
+                .setParameter("primary.address.postal_code", "59004")
+                .setParameter("primary.address.country_code", "US")
+                .setParameter("secondary.name", "Drama Number")
+                .setParameter("secondary.phone", "6464806649")
+                .setParameter("secondary.address.street_line_1", "302 Gorham Ave")
+                .setParameter("secondary.address.city", "Ashland")
+                .setParameter("secondary.address.state_code", "MT")
+                .setParameter("secondary.address.postal_code", "59004")
+                .setParameter("secondary.address.country_code", "US")
                 .setParameter("email_address", "medjalloh1@yahoo.com")
                 .setParameter("ip_address", "108.194.128.165")
                 .build();
@@ -48,8 +48,8 @@ public class IdentityCheckGet {
         TypeReference<Map<String, Object>> typeRef = new TypeReference<Map<String, Object>>() {};
         Map<String, Object> jsonMap = mapper.readValue(response.getEntity().getContent(), typeRef);
 
-        String[] availableChecks = new String[]{"billing_name_checks", "billing_phone_checks", "billing_address_checks",
-                                                "shipping_name_checks", "shipping_phone_checks", "shipping_address_checks",
+        String[] availableChecks = new String[]{"primary_phone_checks", "primary_address_checks",
+                                                "secondary_phone_checks", "secondary_address_checks",
                                                 "email_address_checks", "ip_address_checks"};
 
         // Display check results.
